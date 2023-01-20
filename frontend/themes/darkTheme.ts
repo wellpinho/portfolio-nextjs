526f20a7d8552f1143ff5679e7fa708c13d3d2cb
wellpinho <wellpinho@outlook.com>
2023-01-20 11:38:34 -0300

    dark them

---
+++ frontend/themes/darkTheme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@nextui-org/react"
+
+export const darkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {
+      primary: '#4ADE7B',
+      secondary: '#F9CB80',
+      error: '#FCC5D8',
+    },
+  }
+});
