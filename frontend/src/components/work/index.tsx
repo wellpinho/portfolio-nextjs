1fab61eca29f5fed7e3d1551f399e25e9c7fbc17
wellpinho <wellpinho@outlook.com>
2023-01-25 10:29:01 -0300

    refact work component

--- frontend/src/components/work/index.tsx
+++ frontend/src/components/work/index.tsx
@@ -1,54 +1,6 @@
 import Link from 'next/link';
 import styles from './experience.module.scss'
-
-const Expriences = [
-  {
-    date: "2021 - Present",
-    logo: "/assets/work/ahgora.jpg",
-    position: "Software Engineer",
-    skills:
-      "Docker , GitLab, JIRA, API REST, SOAP, Secure File Transfer Protocol, scrum, Node.js, Vue.js, MongoDB",
-    companyName: "Ahgora Sistemas",
-    workFrom: "(Remote)",
-    link: "https://www.ahgora.com/",
-  },
-  {
-    date: "2021 - 2021",
-    logo: "/assets/work/floripa.jpg",
-    position: "Full Stack Development",
-    skills: "Nextjs, AWS, Node.js, TypeScript, MongoDB",
-    companyName: "Floripa Comercial",
-    workFrom: "(Remote)",
-    link: "/",
-  },
-  {
-    date: "2019 - 2020",
-    logo: "/assets/work/riocom.jpg",
-    position: "Backend developer",
-    skills: "RestAPI NodeJS, Express, TypeORM, SQL.",
-    companyName: "Rio Comercial",
-    workFrom: "(Remote)",
-    link: "/",
-  },
-  {
-    date: "2017 - 2019",
-    logo: "/assets/work/evasofis.jpeg",
-    position: "Full Stack Development",
-    companyName: "Evasofis",
-    skills: "NodeJS, Javascript, Html5, SCSS, SQL",
-    workFrom: "(Hybrid)",
-    link: "/",
-  },
-  {
-    date: "2010 - 2017",
-    logo: "/assets/work/1.png",
-    position: "Freelance",
-    skills: "Javascript, html and CSS, sql",
-    companyName: "Ver & Comprar",
-    workFrom: "(Home work)",
-    link: "/",
-  },
-];
+import { Expriences } from '@/objects'
 
 const WorkExperience = () => {
   return (
@@ -56,9 +8,7 @@ const WorkExperience = () => {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-6 col-12">
-            <div className="tag_title">
-              <h2>My work experiences</h2>
-            </div>
+            <h2 className='tag_title'>My work experiences</h2>
           </div>
         </div>
         <div className={styles.work_wrap}>
@@ -69,18 +19,19 @@ const WorkExperience = () => {
                   <ul>
                     <li className={styles.logo}>
                       <picture>
-                        <img src={exprience.logo} alt="" />
+                        <img src={exprience.logo} alt={exprience.companyName} />
                       </picture>
                     </li>
                     <li className={styles.date}>{exprience.date}</li>
                     <li className={styles.position}>
                       {exprience.position}
-                      <span>{exprience.companyName}
-                        <span>{exprience.skills}</span>
-                        <span>{exprience.workFrom}</span>
-                      </span>
+                      <span className={styles.company}>{exprience.companyName}</span>
+                      <span>{exprience.skills}</span>
+                      <span>{exprience.workFrom}</span>
                     </li>
-                    <li className={styles.link}><Link href="/">Go to website</Link></li>
+                    <li className={styles.link}>
+                      <Link href={exprience.link}>Go to website</Link>
+                    </li>
                   </ul>
                 </div>
               </div>
--- frontend/src/objects/index.ts
+++ frontend/src/objects/index.ts
@@ -15,3 +15,52 @@ export const links = {
   sponsor: 'https://www.paypal.com/donate/?hosted_button_id=DPS2FRCH25ZMJ',
   cv: 'https://docs.google.com/document/d/1WpanENrFqR18fD523jP0882ZiVv0LoClSLV7QeyGirk/edit'
 }
+
+export const Expriences = [
+  {
+    date: "2021 - Present",
+    logo: "/assets/work/ahgora.jpg",
+    position: "Software Engineer",
+    skills:
+      "Docker , GitLab, JIRA, API REST, SOAP, Secure File Transfer Protocol, scrum, Node.js, Vue.js, MongoDB",
+    companyName: "Ahgora Sistemas",
+    workFrom: "(Remote)",
+    link: "https://www.ahgora.com/",
+  },
+  {
+    date: "2021 - 2021",
+    logo: "/assets/work/floripa.jpg",
+    position: "Full Stack Development",
+    skills: "Nextjs, AWS, Node.js, TypeScript, MongoDB",
+    companyName: "Floripa Comercial",
+    workFrom: "(Remote)",
+    link: "/",
+  },
+  {
+    date: "2019 - 2020",
+    logo: "/assets/work/riocom.jpg",
+    position: "Backend developer",
+    skills: "RestAPI NodeJS, Express, TypeORM, SQL.",
+    companyName: "Rio Comercial",
+    workFrom: "(Remote)",
+    link: "/",
+  },
+  {
+    date: "2017 - 2019",
+    logo: "/assets/work/evasofis.jpeg",
+    position: "Full Stack Development",
+    skills: "NodeJS, React, SASS, SQL",
+    companyName: "Evasofis",
+    workFrom: "(Hybrid)",
+    link: "/",
+  },
+  {
+    date: "2010 - 2017",
+    logo: "/assets/work/1.png",
+    position: "Freelance",
+    skills: "Javascript, html and CSS, sql",
+    companyName: "Ver & Comprar",
+    workFrom: "(Home work)",
+    link: "/",
+  },
+];
